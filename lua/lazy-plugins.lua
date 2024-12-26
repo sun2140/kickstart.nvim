41586ae086084698f6728eabbdbf9b77f29559b3
Nicolas GORAJ <github@goraj.fr>
2024-12-26 16:46:49 +0100

    refactor: lot of things

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -1,54 +1,13 @@
 require('lazy').setup({
-    require 'plugins.formatting.tabs-width',
-    require 'plugins.git',
-    require 'plugins.navigation.finder',
-    require 'plugins.navigation.which-key',
-    require 'plugins.languages-servers.lua',
-    require 'plugins.languages-servers.luvit',
-    require 'plugins.languages-servers.all-others-lsps',
     require 'plugins.autocompletion',
-    require 'plugins.visual-aid.highlight-comment',
-    require 'plugins.visual-aid.indent-line',
-    require 'plugins.automation.formatting',
-    require 'plugins.themes.tokyonight',
-    {
-        'echasnovski/mini.nvim',
-        version = '*',
-        config = function()
-            require('mini.ai').setup { n_lines = 500 }
-            require('mini.operators').setup()
-            require('mini.surround').setup()
-            require('mini.icons').setup()
-            require('mini.files').setup()
-            require('mini.pairs').setup()
-
-            vim.keymap.set('n', '<leader>e', function()
-                MiniFiles.open()
-            end, { desc = '[E]xplore filesystem' })
-
-            local statusline = require 'mini.statusline'
-            statusline.setup { use_icons = vim.g.have_nerd_font }
-
-            ---@diagnostic disable-next-line: duplicate-set-field
-            statusline.section_location = function()
-                return '%2l:%-2v'
-            end
-        end,
-    },
-    {
-        'nvim-treesitter/nvim-treesitter',
-        build = ':TSUpdate',
-        main = 'nvim-treesitter.configs',
-        opts = {
-            ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
-            auto_install = true,
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = { 'ruby' },
-            },
-            indent = { enable = true, disable = { 'ruby' } },
-        },
-    },
+    require 'plugins.git',
+    require 'plugins.mini',
+    require 'plugins.theme',
+    require 'plugins.treesitter',
+    { import = 'plugins.formatting' },
+    { import = 'plugins.languages-servers' },
+    { import = 'plugins.navigation' },
+    { import = 'plugins.visual-aid' },
 }, {
     ui = {
         icons = vim.g.have_nerd_font and {} or {
