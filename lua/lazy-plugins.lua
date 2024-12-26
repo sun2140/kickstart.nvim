7e9a1e73ca923ccc635f1f8e9ca73d2b3e1f38ca
Nicolas GORAJ <github@goraj.fr>
2024-12-26 16:09:17 +0100

    refactor: a lot...

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -1,10 +1,8 @@
--- [[ Configure and install plugins ]]
 require('lazy').setup({
     require 'plugins.formatting.tabs-width',
     require 'plugins.git',
     require 'plugins.navigation.finder',
     require 'plugins.navigation.which-key',
-    --require 'plugins.navigation.neo-tree',
     require 'plugins.languages-servers.lua',
     require 'plugins.languages-servers.luvit',
     require 'plugins.languages-servers.all-others-lsps',
@@ -14,89 +12,45 @@ require('lazy').setup({
     require 'plugins.automation.formatting',
     require 'plugins.automation.autopairs',
     require 'plugins.themes.tokyonight',
-    { -- Collection of various small independent plugins/modules
+    {
         'echasnovski/mini.nvim',
+        version = '*',
         config = function()
+            require('mini.ai').setup { n_lines = 500 }
+            require('mini.operators').setup()
+            require('mini.surround').setup()
             require('mini.icons').setup()
             require('mini.files').setup()
 
             vim.keymap.set('n', '<leader>e', function()
-                local minifiles = require 'mini.files'
-                minifiles.open()
+                MiniFiles.open()
             end, { desc = '[E]xplore filesystem' })
 
-            require('mini.ai').setup { n_lines = 500 }
-
-            -- Add/delete/replace surroundings (brackets, quotes, etc.)
-            --
-            -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-            -- - sd'   - [S]urround [D]elete [']quotes
-            -- - sr)'  - [S]urround [R]eplace [)] [']
-            require('mini.surround').setup()
-
-            -- Simple and easy statusline.
-            --  You could remove this setup call if you don't like it,
-            --  and try some other statusline plugin
             local statusline = require 'mini.statusline'
-            -- set use_icons to true if you have a Nerd Font
             statusline.setup { use_icons = vim.g.have_nerd_font }
 
-            -- You can configure sections in the statusline by overriding their
-            -- default behavior. For example, here we set the section for
-            -- cursor location to LINE:COLUMN
             ---@diagnostic disable-next-line: duplicate-set-field
             statusline.section_location = function()
                 return '%2l:%-2v'
             end
-
-            -- ... and there is more!
-            --  Check out: https://github.com/echasnovski/mini.nvim
         end,
     },
-    { -- Highlight, edit, and navigate code
+    {
         'nvim-treesitter/nvim-treesitter',
         build = ':TSUpdate',
-        main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-        -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+        main = 'nvim-treesitter.configs',
         opts = {
             ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
-            -- Autoinstall languages that are not installed
             auto_install = true,
             highlight = {
                 enable = true,
-                -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-                --  If you are experiencing weird indenting issues, add the language to
-                --  the list of additional_vim_regex_highlighting and disabled languages for indent.
                 additional_vim_regex_highlighting = { 'ruby' },
             },
             indent = { enable = true, disable = { 'ruby' } },
         },
-        -- There are additional nvim-treesitter modules that you can use to interact
-        -- with nvim-treesitter. You should go explore a few and see what interests you:
-        --
-        --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-        --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-        --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     },
-
-    -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
-    -- init.lua. If you want these files, they are in the repository, so you can just download them and
-    -- place them in the correct locations.
-
-    --  NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/plugins/*.lua`
-    --    This is the easiest way to modularize your config.
-    --
-    --  Uncomment the following line and add your plugins to `lua/plugins/*.lua` to get going.
-    -- { import = 'plugins' },
-    --
-    -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
-    -- Or use telescope!
-    -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
-    -- you can continue same window with `<space>sr` which resumes last telescope search
 }, {
     ui = {
-        -- If you are using a Nerd Font: set icons to an empty table which will use the
-        -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
         icons = vim.g.have_nerd_font and {} or {
             cmd = '⌘',
             config = '🛠',
